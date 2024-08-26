fdc7c366544bcb1f1a3a12580a71b19da7115931
Aram47 <aram.minasyan.00@gmail.com>
2024-08-26 15:28:02 +0400

    task finished

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {MongoClient} = require('mongodb');
+const client = new MongoClient("mongodb://localhost:27017");
+const dbName = "example";
+const obj = {
+    "name" : "Mike", 
+    "age"  : 25
+};
+
+client.connect();
+const db = client.db(dbName);
+const collection = db.collection('users');
+
+collection.insertOne(obj);
+collection.findOne(obj).then((res) => console.log(res));
